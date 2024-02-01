f6bc4feb1f07d591632871e17b656b89a46957de
Cody Wenrich-Clegg <cwenrich13@gmail.com>
2024-01-31 23:59:00 -0500

    setting up db schema

---
+++ db/group.sql
@@ -0,0 +1,6 @@
+CREATE TABLE groups (
+group_id INTEGER  NOT NULL AUTHO_INCREMENT,
+group_name VARCHAR(100) NOT NULL,
+alt_name VARCHAR[],
+PRIMARY KEY (group_id),
+);
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"database/sql"
+	
+	_"github.com/lib/pq"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	"github.com/joho/godotenv"
+)
+
+
